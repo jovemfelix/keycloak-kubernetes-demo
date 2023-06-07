6b56fb6321b22f39798208d350070941c81e7d1f
Renato Alves Felix <renatofelix.redhat@gmail.com>
2023-06-07 12:06:50 -0300

    cleanup

--- frontend/src/keycloak.php
+++ frontend/src/keycloak.php
@@ -2,9 +2,9 @@
 header('Content-Type: application/json');
 
 $config = array(
-    'realm' => 'demo',
+    'realm' => $_ENV['KEYCLOAK_REALM'] ?: 'demo',
     'auth-server-url' => $_ENV['KEYCLOAK_URL'],
-    'resource' => "app"
+    'resource' => $_ENV['KEYCLOAK_RESOURCE'] ?: "app"
 );
 
 echo json_encode($config, JSON_UNESCAPED_SLASHES);
